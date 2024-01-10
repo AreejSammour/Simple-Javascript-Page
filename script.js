8f13a1b68463a4bb4dcb9f5c6c4ca14f699783fb
Asammour <areej_sammour@hotmail.com>
2024-01-10 16:26:15 +0100

    adding swedish postcode

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const nameInput = document.getElementById('name')
 const emailInput = document.getElementById('email')
 const phoneInput = document.getElementById('phone')
 const contactSection = document.getElementById ('contact')
+const postalCodeId = document.getElementById('PostalCodeId');
+const SwedishPostalCodeInput = document.getElementById('SwedishPostalCode')
+
+// Lägg till EventListener för att visaPostalCode
+emailInput.addEventListener('input', () => {
+  updatePostalCodeVisibility();
+});
 
 contactLink.addEventListener("click", () => {
   getname()
@@ -16,35 +23,50 @@ contactLink.addEventListener("click", () => {
 function getname() {
   let readname = nameInput.value
   console.log("Name : " + readname)
-
   let createParagraphElement = createParagraph("Name : " + readname)
-
   contactSection.appendChild(createParagraphElement); // p kommer att visas under kontakt sektionen
 }
 
 function getEmail() {
-  let readEmail = emailInput.value
-  console.log("Email : " + readEmail)
-
-  let createParagraphElement = createParagraph("Email : " + readEmail)
-
-  contactSection.appendChild(createParagraphElement); // p kommer att visas under kontakt sektionen
+  let readEmail = emailInput.value;
+  console.log("Email : " + readEmail);
+  let createParagraphElement = createParagraph("Email : " + readEmail);
+  contactSection.appendChild(createParagraphElement);
+  updatePostalCodeVisibility();
+  
+  if(postalCodeId.style.display == 'block'){
+    getpostcode()
+  }
 }
 
 function getphone() {
   let readPhone = phoneInput.value
   console.log("Phone : " + readPhone)
-
   let createParagraphElement = createParagraph("Phone : " + readPhone)
   console.log(createParagraphElement);
+  contactSection.appendChild(createParagraphElement); // p kommer att visas under kontakt sektionen
+}
 
+function updatePostalCodeVisibility() {
+  // Kontrollera om mejlet slutar med ".se" och visa/dölj PostalCodeId därefter
+  if (emailInput.value.endsWith('.se')) {
+    postalCodeId.style.display = 'block';
+  } else {
+    postalCodeId.style.display = 'none';
+  }
+}
+
+function getpostcode() {
+  let readSwedishPCInput = SwedishPostalCodeInput.value
+  console.log("Swedish PostalCode : " + readSwedishPCInput)
+  let createParagraphElement = createParagraph("Swedish PostalCode : " + readSwedishPCInput)
+  console.log(createParagraphElement);
   contactSection.appendChild(createParagraphElement); // p kommer att visas under kontakt sektionen
 }
 
 function createParagraph(paragrahpText){
   let paragrahp = document.createElement("p")
   paragrahp.textContent = paragrahpText
-
   paragrahp.style.backgroundColor = "#FAC748"
   paragrahp.style.fontSize = "20px"
   paragrahp.style.margin = "20px"
